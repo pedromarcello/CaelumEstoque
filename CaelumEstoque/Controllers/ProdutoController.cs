3f4bf79c7b26fabbd8ebd8799bee97030cb68133
Pedro <Pedro@LAPTOP-TSMEKDU8>
2016-12-10 02:11:12 -0200

    Add project files.

---
+++ CaelumEstoque/Controllers/ProdutoController.cs
@@ -0,0 +1,34 @@
+﻿using CaelumEstoque.DAO;
+using CaelumEstoque.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaelumEstoque.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        // GET: Produto
+        public ActionResult Index()
+        {
+            ProdutosDAO dao = new ProdutosDAO();
+            IList<Produto> produtos = dao.Lista();
+            ViewBag.Produtos = produtos;
+            return View();
+        }
+        public ActionResult Form()
+        {
+            return View();
+        }
+        public ActionResult Adiciona(Produto produto)
+        {
+            
+            ProdutosDAO dao = new ProdutosDAO();
+            dao.Adiciona(produto);
+            return View();
+
+        }
+    }
+}
